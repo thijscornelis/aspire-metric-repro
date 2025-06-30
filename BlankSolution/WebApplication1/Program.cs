eb7870ba19d5caaf945ae3777620fdb68ad77dc1
Thijs Cornelis <thijs.cornelis@gaming1.com>
2025-06-30 20:15:55 +0200

    chore: setup traces and logs

--- BlankSolution/WebApplication1/Program.cs
+++ BlankSolution/WebApplication1/Program.cs
@@ -1,50 +1,55 @@
-
+using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
-namespace WebApplication1
+namespace WebApplication1;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        var resource =
+            ResourceBuilder.CreateDefault()
+                .AddService("MyCustomServiceName", serviceInstanceId: Environment.MachineName);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddOpenTelemetry().WithMetrics(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var resource =
-                ResourceBuilder.CreateDefault()
-                    .AddService("MyCustomServiceName", serviceInstanceId: Environment.MachineName);
+            options.SetResourceBuilder(resource);
+            options.AddAspNetCoreInstrumentation();
+            options.AddHttpClientInstrumentation();
+            options.AddOtlpExporter();
+        }).WithTracing(options => options.SetResourceBuilder(resource)
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter());
 
-            // Add services to the container.
+        builder.Services.AddLogging(x => x.AddOpenTelemetry(c => c.AddOtlpExporter()));
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        var app = builder.Build();
 
-            builder.Services.AddOpenTelemetry().WithMetrics(options =>
-            {
-                options.SetResourceBuilder(resource);
-                options.AddAspNetCoreInstrumentation();
-                options.AddHttpClientInstrumentation();
-                options.AddOtlpExporter();
-            });
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
